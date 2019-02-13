958a8156f217f3a3d378f843a8a11a14f26b310b
Rowan Wookey <admin@rwky.net>
2019-02-13 15:40:22 +0000

    Added tests for displayName not including middle name #5

--- test/profile.test.js
+++ test/profile.test.js
@@ -40,4 +40,36 @@ describe('Profile.parse', () => {
       expect(profile.photos[0].value).to.equal('http://profile.ak.fbcdn.net/hprofile-ak-prn1/example.jpg');
     });
   });
+
+  describe('profile with middle name', () => {
+    let profile;
+
+    before((done) => {
+      fs.readFile('test/fixtures/middle-name.json', 'utf8', (err, data) => {
+        if (err) { return done(err); }
+        profile = Profile.parse(data);
+        done();
+      });
+    });
+
+    it('should parse profile', () => {
+      expect(profile.displayName).to.equal('First Middle Last');
+    });
+  });
+
+  describe('profile without middle name', () => {
+    let profile;
+
+    before((done) => {
+      fs.readFile('test/fixtures/no-middle-name.json', 'utf8', (err, data) => {
+        if (err) { return done(err); }
+        profile = Profile.parse(data);
+        done();
+      });
+    });
+
+    it('should parse profile', () => {
+      expect(profile.displayName).to.equal('First Last');
+    });
+  });
 });
